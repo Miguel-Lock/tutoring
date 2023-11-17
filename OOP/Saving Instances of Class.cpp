ca492047c2e5cecbf722554a2e5ee5c4e72af26d
Miguel-Lock <miguellock333@gmail.com>
2023-11-17 16:24:10 -0500

    Completed my additions

--- OOP/Saving Instances of Class.cpp
+++ OOP/Saving Instances of Class.cpp
@@ -9,11 +9,10 @@ class Character {
         string name;
         int HP;
         int XP;
-    Character(string n, string r, int hp, int xp) {
+    Character(string n, int hp, int xp) {
         name = n;
         HP = hp;
         XP = xp;
-        characterVector.push_back(*this);
     }
 };
 
@@ -29,32 +28,33 @@ void add_character() {
 
     cout << "Enter a character name: ";
     getline(cin, name);
-    cout << "\nEnter a race: ";
+
+    cout << "Enter HP: ";
     cin >> HP;
-    cout << "\nEnter XP: ";
+
+    cout << "Enter XP: ";
     cin >> XP;
 
     Character temp(name, HP, XP);
-
     characterVector.push_back(temp);
-
-    cout << "Character successfully added!" << endl;
 }
 
 void display_characters() {
     for(int i=0; i<characterVector.size(); i++) {
-        cout << characterVector[i].name << endl;
+        cout << characterVector[i].name << "\nXP: " << characterVector[i].XP << " HP: " << characterVector[i].HP << endl << endl;
     }
 }
 
 int main() {
-    int num_characters;
+    string proceed = "Yes";
 
-    cout << "How many characters do you want to add: ";
-    cin >> num_characters;
-
-    for (int i=0; i<num_characters; i++) {
+    while (proceed != "n" && proceed != "N" && proceed != "No" && proceed != "no") {
         add_character();
+
+        cout << "Would you like to add another character? (Y/N)\n>";
+        cin >> proceed;
+
+        cin.ignore();
     }
 
     display_characters();
