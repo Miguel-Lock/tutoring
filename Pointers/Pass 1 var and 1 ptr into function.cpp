d03b503e7e266335d5361a82bd8aebb5f3d14761
Miguel-Lock <miguellock333@gmail.com>
2023-11-17 09:43:33 -0500

    Added some things about pointers

---
+++ Pointers/Pass 1 var and 1 ptr into function.cpp
@@ -0,0 +1,47 @@
+/*
+Miguel Lock
+Version 1.0
+11-17-23
+
+This file is meant to demonstrate the use of pointers.
+It demonstrates what happens if you pass a variable 
+    and a pointer into a function and edit their values.
+*/
+#include <iostream>
+
+using namespace std;
+
+int fun(int a, int *miguelPtr) {
+    cout << "\n\nIn function fun() before operations\n";
+    cout << "a: " << a << endl;
+    cout << "*miguelPtr: " << *miguelPtr << endl;
+
+    a += 10;
+    *miguelPtr += 10;
+
+    cout << "\n\nIn function fun() after operations. Will return a.\n";
+    cout << "a: " << a << endl;
+    cout << "*miguelPtr: " << *miguelPtr << endl;
+
+    return a;
+}
+
+int main() {
+    int myVar = 1;
+    int *myPtr = new int(2);
+    int myResult = 0;
+
+    cout << "Variables in main before calling fun(myVar, *myPtr):\n";
+    cout << "myVar: " << myVar << endl;
+    cout << "myPtr: " << *myPtr << endl;
+    cout << "myResult: " << myResult << endl;
+
+    myResult = fun(myVar, myPtr);
+
+    cout << "\n\nVariables in main after calling fun(myVar, *myPtr):\n";
+    cout << "myVar: " << myVar << endl;
+    cout << "*myPtr: " << *myPtr << endl;
+    cout << "myResult: " << myResult << endl;
+
+    return 0;
+}
---
+++ Pointers/Pass 2 vars into function.cpp
@@ -0,0 +1,47 @@
+/*
+Miguel Lock
+Version 1.0
+11-17-23
+
+This file is meant to demonstrate the use of variables.
+It demonstrates what happens if you pass two variables
+    into a function and edit their values.
+*/
+#include <iostream>
+
+using namespace std;
+
+int fun(int a, int b) {
+    cout << "\n\nIn function fun() before operations\n";
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+
+    a += 10;
+    b += 10;
+
+    cout << "\n\nIn function fun() after operations. Will return a.\n";
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+
+    return a;
+}
+
+int main() {
+    int firstVar = 1;
+    int secondVar = 2;
+    int result = 0;
+
+    cout << "Variables in main before calling fun(firstVar, secondVar):\n";
+    cout << "firstVar: " << firstVar << endl;
+    cout << "secondVar: " << secondVar << endl;
+    cout << "result: " << result << endl;
+
+    result = fun(firstVar, secondVar);
+
+    cout << "\n\nVariables in main after calling fun(firstVar, secondVar):\n";
+    cout << "firstVar: " << firstVar << endl;
+    cout << "secondVar: " << secondVar << endl;
+    cout << "result: " << result << endl;
+
+    return 0;
+}
